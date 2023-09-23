0ad2c0f35238d01f75efe52980d47195494a52e0
Trisha Ravindran <s4004055@student.rmit.edu.au>
2023-09-23 22:49:58 +1000

    second edit made

---
+++ Profile_Set-Up_Trisha/Profile.js
@@ -0,0 +1,8 @@
+// validate data in form here
+//  1. Profile section avatar using JS and can edit and choose username?
+
+//  2. The bottom nav to move across the different set up pages
+
+//  3. Validate data and link some things a database. Eg; users information
+
+//  4. Add an astrik to all required input values
